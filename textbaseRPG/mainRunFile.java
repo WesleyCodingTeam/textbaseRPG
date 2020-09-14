60d125ba1349c665755d325b7baa99e1f7210b57
JaeyoungShin2003 <71117066+JaeyoungShin2003@users.noreply.github.com>
2020-09-14 19:46:03 +0700

    Update mainRunFile.java

--- textbaseRPG/mainRunFile.java
+++ textbaseRPG/mainRunFile.java
@@ -3,7 +3,6 @@ public class mainRunFile{
     public static void main(String[] args){
         //intro currrently disabled for testing of part 1
         //intro.introduction();
-        //pp
         part1.questFGame();
     } 
 }
