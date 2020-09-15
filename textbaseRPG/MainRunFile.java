b2b216c98c0244a428009f0510f8cfabbd72f635
Jaeyoung <71117066+JaeyoungShin2003@users.noreply.github.com>
2020-09-15 18:46:28 +0700

    Update and rename mainRunFile.java to MainRunFile.java

--- textbaseRPG/MainRunFile.java
+++ textbaseRPG/MainRunFile.java
@@ -1,5 +1,5 @@
 // this file is the main file that runs the game
-public class mainRunFile{
+public class MainRunFile{
     public static void main(String[] args){
         //intro currrently disabled for testing of part 1
         //Intro.introduction();
