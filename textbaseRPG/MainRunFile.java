ca4e7ef09ae478ecb98a01a99b3f98cb6963f3f4
JaeyoungShin2003 <jaeyoungshin03@gmail.com>
2020-10-04 20:15:31 +0700

    very very small change

--- textbaseRPG/MainRunFile.java
+++ textbaseRPG/MainRunFile.java
@@ -1,10 +1,11 @@
 // this file is the main file that runs the game
 public class MainRunFile{
     public static void main(String[] args){
-        //Intro.introduction();
+        Intro.introduction();
         //Part1 disabled for the test of item. Just activate it by removing //
         //Part1.questFGame();
         //buttom code is just for inventory testing
+        /*
         Items.itemGeneration();
         Inventory.setInventoryItem(1001, 1);
         Inventory.setInventoryItem(3001, 2);
@@ -12,5 +13,6 @@ public class MainRunFile{
         Character.makeStats("Jaeyoung", "None", 1, 13, 100, 10, 10, 100, 100, 100, 100, 10);
         Monster.generateMonster();
         Battle.battleNow(101);
+        */
     } 
 }
