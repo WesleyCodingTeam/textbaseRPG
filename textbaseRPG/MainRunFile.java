69e8255b555fac81149abfdf656156707afad2a0
JaeyoungShin2003 <jaeyoungshin03@gmail.com>
2021-02-14 16:25:16 +0700

    small change to merge

--- textbaseRPG/MainRunFile.java
+++ textbaseRPG/MainRunFile.java
@@ -1,10 +1,7 @@
 // this file is the main file that runs the game
 public class MainRunFile{
     public static void main(String[] args){
-
         Program.thingsToRunInBeginning();
-        Program.testCharacterGeneration();
-        Part1.questFGame();
-
+        Intro.introduction();
     }
 }
