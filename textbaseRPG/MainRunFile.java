e3120420010b5c92dc23908bbf29e2ee333bb2ee
Jaeyoung <71117066+JaeyoungShin2003@users.noreply.github.com>
2021-02-19 14:42:31 +0700

    quick change

--- textbaseRPG/MainRunFile.java
+++ textbaseRPG/MainRunFile.java
@@ -1,12 +1,10 @@
 // this file is the main file that runs the game
 public class MainRunFile{
     public static void main(String[] args){
+        //TO-DO
         //need to implement equipment system
+        //need to adjust item stats cookie and cake and more item need to be added or adjusted
         Program.thingsToRunInBeginning();
-        Program.testCharacterGeneration();
-        MainCharacter.coin = 400;
-        Inventory.setInventoryItem(4001, 50);
-        Inventory.setInventoryItem(4002, 50);
-        Action.charAction();
+        Intro.introduction();
     }
 }
